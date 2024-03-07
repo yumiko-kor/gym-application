134318146cb8744d209568b5784ccdc94959160c
Yu <yugyeongjung@gmail.com>
2024-03-08 01:35:07 +0900

    feat: 환경설정 완료

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
+// 더 간단하게 불러올 수 있는 path
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+  }
 })
