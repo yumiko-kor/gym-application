09851b28940f9b49f73762c27d93c0cd06eaddc0
Yu <yugyeongjung@gmail.com>
2024-03-24 09:22:11 +0900

    프로젝트 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
-
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
+import OurClasses from "./scenes/ourClasses";
+import ContactUs from "./scenes/contactUs";
+import Footer from "./scenes/footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -35,7 +37,9 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage}/>
       <Benefits setSelectedPage={setSelectedPage} />
-
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   )
 }
